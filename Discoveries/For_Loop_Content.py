a1db382ce64ff772db45fa170072039e94a23bee
Mitch <75135053+MitchTheMairal@users.noreply.github.com>
2021-04-29 22:52:16 +0800

    Miscellaneous

---
+++ Discoveries/For_Loop_Content.py
@@ -0,0 +1,7 @@
+# @client.event
+# async def on_message(message):
+#     if (message.author.bot):
+#         return
+
+#     if (message.content.lower() == 'hi' or 'hello' or 'ey' or 'hey'):
+#         await message.channel.send('Hi')
---
+++ Discoveries/Message_Includes.py
@@ -0,0 +1,6 @@
+# @client.event
+# async def pog(Message):
+#     if (Message.have == 'pog'):
+#         await Message.add_reaction('🅿')
+#         await Message.add_reaction('🅿')
+#         await Message.add_reaction('🅿')
